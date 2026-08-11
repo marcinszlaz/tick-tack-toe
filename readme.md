cb8fbb5ee14919a8bf4b61c828a925bc38b7026a
marcin_szlaz <marcinszlaz@gmail.com>
2026-08-11 20:21:26 +0200

    First commit, for my professional ticktacktoe game xD

---
+++ readme.md
@@ -0,0 +1,8 @@
+# Third Portfolio Project
+-------------------------
+* ticktacktoe game (ASCI)
+
+
+## Usefull commands
+* `gh repo create https://github.com/marcinszlaz/tick-tack-toe.git --public` - create repo on github,
+* `git init` , `git brach -M main`, 
---
+++ ticktacktoe.py
@@ -0,0 +1,52 @@
+import os
+import time
+
+
+def clear_screen():
+    os.system('cls' if os.name=='nt' else 'clear')
+
+d = dict(a1=' _ ', a2=' _ ', a3=' _ ',
+         b1=' _ ', b2=' _ ', b3=' _ ',
+         c1=' _ ', c2=' _ ', c3=' _ ')
+
+game_board = (f"\n" 
+f"   A   B   C \n"
+f" 1{d['a1']}|{d['b1']}|{d['c1']}\n"
+f"  ---|---|---\n"
+f" 2{d['a2']}|{d['b2']}|{d['c2']}\n"
+f"  ---|---|---\n"
+f" 3{d['a3']}|{d['b3']}|{d['c3']}\n"
+f"")
+
+# intended to use with .format() function
+game_board_ft = ("\n" 
+"   A   B   C \n"
+" 1{a1}|{b1}|{c1}\n"
+"  ---|---|---\n"
+" 2{a2}|{b2}|{c2}\n"
+"  ---|---|---\n"
+" 3{a3}|{b3}|{c3}\n")
+
+clear_screen()
+print(f"Welcome in Tic-Tac-Toe game \n"
+     f"Type a1, b3, etc. to fill the board.\n"
+     f"Type `quite` to quit\n\n"
+     f"{game_board}")
+
+init = 0
+user_choose = 'no_quit!'
+while user_choose != 'quit':
+    if init % 2 == 0:  
+        user_choose = input("Player `X` turn! \n")
+        d[user_choose] = ' X '
+    else:
+        user_choose = input("Player `Y` turn! \n")
+        d[user_choose] = ' O '
+    clear_screen()
+    print(game_board_ft.format(**d))
+    init += 1
+
+
+
+
+
